eff10b17a02e7e3d52b281e0d1f610b097a89056
ailinvm <ailinvm0@chicagobooth.edu>
2022-07-03 23:56:40 +0000

    Rock bones skeleton of rock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,13 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+def play_rock
+ #write code to do what the user needs us to do
+ #redirect_to("https://www.wikipedia.org")
+
+ #render({ :plain => "Hello, World!" })
+
+ render({ :template => "game_templates/user_rock.html.erb" })
+end
 
 end
